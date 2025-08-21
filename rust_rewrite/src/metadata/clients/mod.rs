932147791b9b6833e9abc0b0af00d06582dbdb4e
amon2126 <carlo.ruggiero@live.it>
2025-08-21 17:15:30 +0200

    feat: add Rust migration plan and metadata client modules

---
+++ rust_rewrite/src/metadata/clients/mod.rs
@@ -0,0 +1,11 @@
+pub mod simkl;
+pub mod tmdb;
+pub mod tvdb;
+pub mod imdb;
+pub mod mal;
+
+pub use simkl::SimklClient;
+pub use tmdb::TmdbClient;
+pub use tvdb::TvdbClient;
+pub use imdb::ImdbClient;
+pub use mal::MalClient;
